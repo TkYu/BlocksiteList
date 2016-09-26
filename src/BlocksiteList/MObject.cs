32e647f3387f5f2d3906e2b60c9771551be319d2
TsungKang <tsungkang@live.cn>
2016-09-26 01:42:33 -0700

    shrink ips

--- src/BlocksiteList/MObject.cs
+++ src/BlocksiteList/MObject.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlocksiteList
 {
+    public class MapObject
+    {
+        public List<string> urlWithQuery { get; set; } = new List<string>();
+        public List<string> urlWithHttps { get; set; } = new List<string>();
+        public List<string> pureIPAddress { get; set; } = new List<string>();
+        public List<string> pureIPAddressWithPort { get; set; } = new List<string>();
+        public List<string> pureHost { get; set; } = new List<string>();
+    }
+    public class ReduceObject
+    {
+        public Dictionary<string, int> Tokeniz { get; set; } = new Dictionary<string, int>();
+        public List<string> Result { get; set; } = new List<string>();
+    }
     public class MObject
     {
         public MObject(string domain,string date,int percent,string[] tags)
--- src/BlocksiteList/Program.cs
+++ src/BlocksiteList/Program.cs
@@ -9,17 +9,20 @@ using System.Threading.Tasks;
 
 namespace BlocksiteList
 {
-    public class Program
+    public static class Program
     {
-        const string fuck = @"[Greatfirelist]
+        #region var
+        private const string fuck = @"[AutoProxy]
 ! Expires: 7d
 ! Title: Greatfire List
-! fu(o)c(r)k from https://github.com/gfwlist/tinylist
+! Fu(o)c(r)k from https://github.com/gfwlist/tinylist
 ! Last Modified: {0}
 !
 ! HomePage: https://github.com/TkYu/BlocksiteList
 ! License: https://github.com/TkYu/BlocksiteList/blob/master/LICENSE
 !
+! Okay let's go
+
 !##############General List Start###############
 {1}
 !##############General List End#################
@@ -27,18 +30,57 @@ namespace BlocksiteList
 !##############Greatfire List Start#################
 {2}
 !##############Greatfire List End#################
+
+!################White List Start################
+@@||aliyun.com
+@@||baidu.com
+@@||bing.com
+@@||bt.byr.cn
+@@||chinaso.com
+@@|http://nrch.culture.tw/
+@@||fonts.googleapis.com
+@@||cn.gravatar.com
+@@||csi.gstatic.com
+@@||fonts.gstatic.com
+@@||haosou.com
+@@||jd.com
+@@||jike.com
+@@|http://translate.google.cn
+@@|http://www.google.cn/maps
+@@||http2.golang.org
+@@||gov.cn
+@@||bt.neu6.edu.cn
+@@||qq.com
+@@||sina.cn
+@@||sina.com.cn
+@@||sogou.com
+@@||so.com
+@@||soso.com
+@@||taobao.com
+@@||weibo.com
+@@||yahoo.cn
+@@||youdao.com
+@@||zhongsou.com
+@@|http://ime.baidu.jp
+!################White List End##################
 ";
 
         private static readonly string[] deepBlackList =
         {
+            "appspot.com",
             "blogspot.com",
+            "chrome.com",
             "dropbox.com",
             "facebook.com",
             "fbcdn.net",
             "ggpht.com",
             "gmodules.com",
             "google.com",
+            "google.co.jp",
+            "google.co.kr",
             "google.com.hk",
+            "google.com.sg",
+            "google.com.tw",
             "googleapis.com",
             "googleusercontent.com",
             "instagram.com",
@@ -49,9 +91,12 @@ namespace BlocksiteList
             "scmp.com"
         };
 
-        const string fucker = @"https://en.greatfire.org/search/blocked?page=";
+        private const string google = @"
+/^https?:\/\/([^\/]+\.)*google\.(ac|ad|ae|al|am|as|at|az|ba|be|bf|bg|bi|bj|bs|bt|by|ca|cat|cd|cf|cg|ch|ci|cl|cm|co.ao|co.bw|co.ck|co.cr|co.id|co.il|co.in|co.jp|co.ke|co.kr|co.ls|co.ma|com|com.af|com.ag|com.ai|com.ar|com.au|com.bd|com.bh|com.bn|com.bo|com.br|com.bz|com.co|com.cu|com.cy|com.do|com.ec|com.eg|com.et|com.fj|com.gh|com.gi|com.gt|com.hk|com.jm|com.kh|com.kw|com.lb|com.ly|com.mm|com.mt|com.mx|com.my|com.na|com.nf|com.ng|com.ni|com.np|com.om|com.pa|com.pe|com.pg|com.ph|com.pk|com.pr|com.py|com.qa|com.sa|com.sb|com.sg|com.sl|com.sv|com.tj|com.tr|com.tw|com.ua|com.uy|com.vc|com.vn|co.mz|co.nz|co.th|co.tz|co.ug|co.uk|co.uz|co.ve|co.vi|co.za|co.zm|co.zw|cv|cz|de|dj|dk|dm|dz|ee|es|fi|fm|fr|ga|ge|gg|gl|gm|gp|gr|gy|hk|hn|hr|ht|hu|ie|im|iq|is|it|je|jo|kg|ki|kz|la|li|lk|lt|lu|lv|md|me|mg|mk|ml|mn|ms|mu|mv|mw|mx|ne|nl|no|nr|nu|org|pl|pn|ps|pt|ro|rs|ru|rw|sc|se|sh|si|sk|sm|sn|so|sr|st|td|tg|tk|tl|tm|tn|to|tt|us|vg|vn|vu|ws)\/.*/";
 
-        static readonly string[] elastic =
+        private const string fucker = @"https://en.greatfire.org/search/blocked?page=";
+
+        private static readonly string[] elastic =
         {
             @"http://192.168.10.1:9200/_analyze?analyzer=ik_smart&pretty=false&text=",
             @"http://192.168.10.9:9200/_analyze?analyzer=ik_smart&pretty=false&text=",
@@ -59,10 +104,13 @@ namespace BlocksiteList
             @"http://192.168.10.11:9200/_analyze?analyzer=ik_smart&pretty=false&text="
         };
 
+        private static readonly System.Text.RegularExpressions.Regex rx = new System.Text.RegularExpressions.Regex(@"^[\d\.:]+$", System.Text.RegularExpressions.RegexOptions.Compiled);
         private static bool useLocal;
+        #endregion
         public static void Main(string[] args)
         {
-            int pageSize = 0;
+            
+            var pageSize = 0;
             if (Directory.Exists("local"))
             {
                 pageSize = Directory.GetFiles("local", "*.txt", SearchOption.TopDirectoryOnly).Length - 1;
@@ -83,14 +131,162 @@ namespace BlocksiteList
 
             if (pageSize == 0) return;
             Console.WriteLine("Task Start");
+            var lst = Prepare(pageSize);
+            Console.WriteLine("Take Finished\nStart MR");
+            var result = MapReduce(lst);
+            Console.WriteLine("MR Finished\nWrite File...");
+            result.Save();
+            Console.WriteLine("All Finished");
+        }
+        
+
+
+
+        #region MR
+        public static Tuple<string, string> MapReduce(MObject[] list)
+        {
+            Console.WriteLine("Start Map");
+            var map = Map(list);
+            Console.WriteLine("Map Finished\nStart Reduce");
+            var reduce = Reduce(map, Math.Min(Environment.ProcessorCount, elastic.Length*2));
+            Console.WriteLine("Reduce Finished\nStart Finalize");
+            return Finalize(reduce);
+        }
+        public static void Save(this Tuple<string, string> result)
+        {
+            File.WriteAllText("O_BlackWords.txt", result.Item1);
+            File.WriteAllText("O_greatfirelist.txt", result.Item2);
+            File.WriteAllText("BlackWords.txt", string.Join("\n", Split(Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(result.Item1)),64)));
+            File.WriteAllText("greatfirelist.txt", string.Join("\n", Split(Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(result.Item2)),64)));
+        }
+        public static MapObject Mapping(this MObject[] list)
+        {
+            var ret = new MapObject();
+            foreach (var url in list)
+            {
+                var split = url.Domain.Split('/');
+                if (split.Length == 1)
+                {//domain
+                    if (!url.Domain.Contains("%"))
+                    {
+                        if (rx.IsMatch(url.Domain))
+                        {
+                            if (url.Domain.Contains(":"))
+                                ret.pureIPAddressWithPort.Add(url.Domain);
+                            else
+                                ret.pureIPAddress.Add(url.Domain);
+                        }
+                        else
+                        {
+                            ret.pureHost.Add(url.Domain);
+                        }
+                    }
+                }
+                else if (split.Length == 3 && url.Domain.StartsWith("https"))
+                {//domain with scheme
+                    ret.urlWithHttps.Add(split[2]);
+                }
+                else if (split.Length == 3 && url.Domain.StartsWith("http"))
+                {//domain with scheme
+                    //urlWithHttp.Add(split[2]);
+                    throw new Exception("wocao");
+                }
+                else
+                {//url address
+                    var tail = split[split.Length - 1];
+                    if (!ret.urlWithQuery.Contains(tail)) ret.urlWithQuery.Add(tail);
+                }
+            }
+            return ret;
+        }
+        public static MapObject Combine(this MapObject mo)
+        {
+            mo.urlWithHttps = mo.urlWithHttps.Distinct().ToList();
+            mo.pureIPAddressWithPort = mo.pureIPAddressWithPort.Distinct().ToList();
+            mo.pureHost = mo.pureHost.Distinct().ToList();
+            var ipdistinct = mo.pureIPAddress.Distinct().ToDictionary(k=>k, v =>
+            {
+                var split = v.Split('.');
+                return $"{split[0]}.{split[1]}.{split[2]}";
+            });
+            var ipCounter = ipdistinct.GroupBy(c => c.Value).ToDictionary(k => k.Key, v => v.Count());
+            var filtered = ipCounter.Where(c => c.Value > 10).Select(c=>c.Key).ToArray();
+            var result = (from ip in ipdistinct
+                          where !filtered.Contains(ip.Value)
+                          select ip.Key).ToList();
+            result.AddRange(filtered.Select(c => $"||{c}.*^"));
+            mo.pureIPAddress = result;
+            return mo;
+        }
+        public static Tuple<string,string> Finalize(ReduceObject ro)
+        {
+            var BlackWords = string.Join("\n", ro.Tokeniz.OrderByDescending(c => c.Value).Select(c => $"词语：{c.Key}；词频：{c.Value}"));
+            var resultContent = string.Format(fuck, 
+                DateTime.Now.ToString("R"), 
+                string.Join("\n", deepBlackList.Select(c => $"||{c}")) + google,
+                string.Join("\n", ro.Result));
+            resultContent = resultContent.Replace("\r", "").Replace("[AutoProxy]", $"[AutoProxy]\n! Checksum: {CalcSum(resultContent)}");
+            return new Tuple<string, string>(BlackWords, resultContent);
+        }
+        public static MapObject Map(MObject[] list)
+        {
+            return list.Mapping().Combine();
+        }
+        public static ReduceObject Reduce(MapObject mo, int? limit = null)
+        {
+            var result = new ReduceObject();
+            Console.WriteLine("Merge");
+            result.Result.AddRange(mo.pureHost.Select(c => c.StartsWith("www.") ? $"||{c.Substring(4, c.Length - 4)}" : $"||{c}"));
+            foreach (var item in mo.urlWithHttps)
+            {
+                if(mo.pureHost.All(c => c != item))
+                    result.Result.Add("|https://" + item);
+            }
+            Console.WriteLine("Take BlackWords");
+            var tokeniz = new ConcurrentDictionary<string, int>();
+            Parallel.ForEach(mo.urlWithQuery, new ParallelOptions { MaxDegreeOfParallelism = limit ?? Environment.ProcessorCount }, (query, loopState) =>
+            {
+                var dec = query.UrlDecode();
+                if (string.IsNullOrWhiteSpace(dec)) return;
+                if (dec.Any(c => c > 127))
+                {//with chinese path
+                    try
+                    {
+                        var r = Tokeniz(query, System.Threading.Thread.CurrentThread.ManagedThreadId);
+                        foreach (var t in r.tokens.Where(c => c.type == "CN_WORD" || c.type == "ENGLISH"))
+                            tokeniz.AddOrUpdate(t.token, 1, (id, count) => count + 1);
+                    }
+                    catch
+                    {
+                        //TODO excuseme?
+                    }
+                }
+                else
+                {
+                    //var spl = dec.Split(' ');
+                    //foreach (var s in spl)
+                    //{
+                    //    result.Tokeniz.AddOrUpdate(s, 1, (id, count) => count + 1);
+                    //}
+                }
+            });
+            result.Tokeniz = new Dictionary<string, int>(tokeniz);
+            return result;
+        }
+        #endregion
+
+        #region Methods
+
+        private static MObject[] Prepare(int pageSize, int? limit = null)
+        {
             var lst = new ConcurrentBag<MObject>();
-            Parallel.For(0, pageSize, new ParallelOptions {MaxDegreeOfParallelism = 24}, (i, loopState) =>
+            Parallel.For(0, pageSize, new ParallelOptions {MaxDegreeOfParallelism = limit ?? Environment.ProcessorCount}, (i, loopState) =>
             {
                 Console.WriteLine("Working on page {0}", i);
                 try
                 {
                     var content = Take(i); //sync
-                    var items = Parse(content);
+                    var items = Parse(content).Where(c => c.Percent > 75);
                     foreach (var mObject in items)
                         lst.Add(mObject);
                 }
@@ -99,71 +295,14 @@ namespace BlocksiteList
                     //TODO hehe
                 }
             });
-            Console.WriteLine("Take Finished\nStart Analysis");
-            var result = new ConcurrentBag<string>();
-            var tokeniz = new ConcurrentDictionary<string, int>();
-            Parallel.ForEach(lst, new ParallelOptions { MaxDegreeOfParallelism = 12 }, (item, loopState) =>
-            {
-                if (item.Percent < 50) return;
-                var split = item.Domain.Split('/');
-                if (split.Length == 1)
-                {//domain
-                    result.Add(item.Domain);
-                }
-                else if (split.Length == 3 && item.Domain.StartsWith("http"))
-                {//domain with scheme
-                    result.Add("|" + item.Domain);
-                }
-                else
-                {//url address
-                    var tail = split[split.Length - 1];
-                    if (tail.Contains("%") && tail.Length > 6)
-                    {//with path
-                        var dec = tail.UrlDecode();
-                        if (dec.Any(c => c > 127))
-                        {//with chinese path
-                            try
-                            {
-                                var r = Tokeniz(tail, System.Threading.Thread.CurrentThread.ManagedThreadId);
-                                foreach (var t in r.tokens.Where(c => c.type == "CN_WORD"))
-                                    tokeniz.AddOrUpdate(t.token, 1, (id, count) => count + 1);
-                            }
-                            catch
-                            {
-                                //TODO excuseme?
-                            }
-                        }
-                        else
-                        {//without chinese path
-                            //Too mach, drop
-                            //result.Add("||" + item.Domain + "|");
-                        }
-
-                    }
-                    else
-                    {//others
-                        //Too mach, drop
-                        //result.Add("||" + item.Domain);
-                    }
-                }
-            });
-            Console.WriteLine("Analysis Done");
-            var blackWords = string.Join("\n", tokeniz.OrderByDescending(c => c.Value).Select(c => $"词语：{c.Key}；词频：{c.Value}"));
-            File.WriteAllText("O_BlackWords.txt", blackWords);
-            var resultContent = string.Format(fuck, DateTime.Now.ToString("R"), string.Join("\n", deepBlackList.Select(c => $"||{c}")), string.Join("\n", result));
-            resultContent = resultContent.Replace("[Greatfirelist]", $"[Greatfirelist]\n! Checksum: {CalcSum(resultContent)}");
-            File.WriteAllText("O_greatfirelist.txt", resultContent);
-
-            File.WriteAllText("BlackWords.txt", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(blackWords)));
-            File.WriteAllText("greatfirelist.txt", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(resultContent)));
-            Console.WriteLine("All Finished");
+            return lst.ToArray();
         }
 
-        private static ElasticResult Tokeniz(string input,int threadid)
+        private static ElasticResult Tokeniz(string input, int threadid)
         {
             using (var hc = new HttpClient())
             {
-                int fp = threadid%elastic.Length;
+                int fp = threadid % elastic.Length;
                 var result = hc.GetStringAsync(elastic[fp] + input).ConfigureAwait(false).GetAwaiter().GetResult();
                 return Newtonsoft.Json.JsonConvert.DeserializeObject<ElasticResult>(result);
             }
@@ -228,10 +367,15 @@ namespace BlocksiteList
         }
         private static string CalcSum(string content)
         {
-            //var clean = System.Text.RegularExpressions.Regex.Replace(content, @"! Checksum:.*\n", "").Replace("\r", "").Replace("\n\n", "\n");
-            var clean = content.Replace("\r", "").Replace("\n\n", "\n");
+            var clean = System.Text.RegularExpressions.Regex.Replace(content, @"! Checksum:.*\n", "").Replace("\r", "").Replace("\n\n", "\n");
+            //var clean = content.Replace("\r", "").Replace("\n\n", "\n");
             using (var sha1 = System.Security.Cryptography.MD5.Create())
                 return Convert.ToBase64String(sha1.ComputeHash(System.Text.Encoding.UTF8.GetBytes(clean))).TrimEnd('=');
+        }
+
+        private static IEnumerable<string> Split(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize).Select(i => str.Substring(i * chunkSize, chunkSize));
         }
 
         private static bool IsRunningOnCore()
@@ -239,5 +383,6 @@ namespace BlocksiteList
             var fileName = Path.GetFileNameWithoutExtension(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
             return fileName == "dotnet";
         }
+        #endregion
     }
 }
